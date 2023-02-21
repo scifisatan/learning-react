17ab85020ea1c40357830433bbd1561c06cbf96b
scifisatan <brawlershere@gmail.com>
2023-02-21 23:38:21 +0545

    learned states

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,26 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import React,{useState} from 'react';
 
 function ExpenseItem(props) {
+  var i=1;
+  const [title,setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate
         date={props.date}
-        title={props.title}
-        amount={props.amount}
       />
       <div className="expense-item__decription">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick = {clickHandler}>Click me</button>
     </Card>
   );
 }
